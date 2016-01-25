3a9f82473c7d52c3227857f49426be3fdb939601
liam <jumpeiliamyamane@gmail.com>
2016-01-25 19:53:38 +0900

    Update hello.js

--- app/hello.js
+++ app/hello.js
@@ -1,10 +1,5 @@
 function hello(name) {
-/*
-* ここにnameに"World"が入力された時、この関数が"Hello World!"と返すような実装をします。
-* nameが"codecheck"の場合には、"Hello codecheck!"が返されます。
-*/
-  //ここにHelloworldを実装します。
-  //return "Hello World!";
+return "Hello World!!!!!";
 }
 
 module.exports = hello;
